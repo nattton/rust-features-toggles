3d83baa626395b9cedb23b5ca8015f72325f67af
Nattapong Tonprasert <nattapong.ton@ttbbank.com>
2024-12-09 09:38:23 +0700

    add default database_url from current_dir

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,17 @@ use std::env;
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    SqliteConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+    let database_url = env::var("DATABASE_URL");
+    match database_url {
+        Ok(database_url) => SqliteConnection::establish(&database_url)
+            .unwrap_or_else(|_| panic!("Error connecting to {}", database_url)),
+        Err(_) => {
+            let path = env::current_dir().expect("cannot get current dir");
+            let database_url = format!("{}/database.db", path.display());
+            SqliteConnection::establish(&database_url)
+                .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+        }
+    }
 }
 
 pub fn create_feature(conn: &mut SqliteConnection, feature: &NewFeature) {
